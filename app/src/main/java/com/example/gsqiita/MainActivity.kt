616cf2aeb54ad330fee9b5d5c8b4578c492a2934
Taro Nagasawa <taro.nagasawa@gmail.com>
2017-08-09 16:59:51 +0900

    UIまで作成

--- app/src/main/java/com/example/gsqiita/MainActivity.kt
+++ app/src/main/java/com/example/gsqiita/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.gsqiita
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
+import com.example.gsqiita.model.Article
+import com.example.gsqiita.model.User
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val article = Article(id = "123",
+                title = "Kotlin入門",
+                url = "http://www.example.com/articls/123",
+                user = User(id = "456", name = "たろう", profileImageUrl = ""))
+
+        val listAdapter = ArticleListAdapter(this)
+        listAdapter.articles = listOf(article)
+
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val article = listAdapter.articles[position]
+            ArticleActivity.intent(this, article).let { startActivity(it) }
+        }
     }
 }
---
+++ app/src/main/java/com/example/gsqiita/model/Article.kt
@@ -0,0 +1,37 @@
+package com.example.gsqiita.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readParcelable(User::class.java.classLoader)) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(title)
+        parcel.writeString(url)
+        parcel.writeParcelable(user, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Article> {
+        override fun createFromParcel(parcel: Parcel): Article {
+            return Article(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Article?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
